9926babeda976f0541ea88504d90d4820b66f5ba
Jeevana Kalvakuntla <jeka2259@colorado.edu>
2026-01-30 17:48:21 -0700

    Update seller_server.py

--- seller_server.py
+++ seller_server.py
@@ -1,28 +1,45 @@
 import socket, threading
 from protocol import send, recv
 
-CUST=('localhost',9002)
-PROD=('localhost',9001)
-HOST,PORT='0.0.0.0',8001
+CUST = ('localhost', 9002)
+PROD = ('localhost', 9001)
+HOST, PORT = '0.0.0.0', 8001
 
-
-def db(addr,msg):
-    s=socket.socket(); s.connect(addr)
-    send(s,msg); r=recv(s); s.close(); return r
-
+def db(addr, msg):
+    s = socket.socket(); s.connect(addr)
+    send(s, msg); r = recv(s); s.close()
+    return r
 
 def handle(conn):
     with conn:
         while True:
-            req=recv(conn)
+            req = recv(conn)
             if not req: break
-            v=db(CUST,{'type':'VALIDATE','sid':req['sid']})
-            if v['status']!='OK': send(conn,{'status':'ERR'}); continue
-            if req['type']=='ADD_ITEM': send(conn, db(PROD, req))
-            else: send(conn,{'status':'ERR'})
 
+            v = db(CUST, {'type':'VALIDATE','sid':req['sid']})
+            if v['status']!='OK':
+                send(conn, {'status':'ERR'})
+                continue
 
-s=socket.socket(); s.bind((HOST,PORT)); s.listen()
-print('Seller Server running')
+            user = v['user']
+
+            if req['type']=='ADD_ITEM':
+                send(conn, db(PROD, {**req, 'seller':user}))
+
+            elif req['type']=='UPDATE_PRICE':
+                send(conn, db(PROD, req))
+
+            elif req['type']=='UPDATE_QTY':
+                send(conn, db(PROD, req))
+
+            elif req['type']=='GET_SELLER_RATING':
+                send(conn, db(CUST, {'type':'GET_SELLER_RATING','seller':user}))
+
+            else:
+                send(conn, {'status':'ERR'})
+
+s = socket.socket(); s.bind((HOST,PORT)); s.listen()
+print("Seller Server running")
 while True:
-    c,_=s.accept(); threading.Thread(target=handle,args=(c,),daemon=True).start()
+    c,_=s.accept()
+    threading.Thread(target=handle,args=(c,),daemon=True).start()
