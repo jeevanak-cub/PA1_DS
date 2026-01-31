c2d6f73477f6b8731b7c44ab1d125a1359d110a6
Jeevana Kalvakuntla <jeka2259@colorado.edu>
2026-01-30 17:47:45 -0700

    Update customer_db.py

--- customer_db.py
+++ customer_db.py
@@ -1,87 +1,91 @@
 import socket, threading, time, uuid
 from protocol import send, recv
 
-buyers = {}
-sellers = {}
-sessions = {}
-carts = {}
-purchases = {}
+buyers, sellers = {}, {}
+sessions, carts, purchases = {}, {}, {}
+seller_feedback = {}
 LOCK = threading.Lock()
 TIMEOUT = 300
 HOST, PORT = "0.0.0.0", 9002
 
-
-def cleanup_sessions():
+def cleanup():
     while True:
         time.sleep(30)
         with LOCK:
             now = time.time()
-            for sid in list(sessions.keys()):
+            for sid in list(sessions):
                 if now - sessions[sid]['last'] > TIMEOUT:
                     del sessions[sid]
-
-
-def handle(conn):
-    with conn:
-        while True:
-            req = recv(conn)
-            if not req:
-                break
-            send(conn, process(req))
-
 
 def process(req):
     with LOCK:
         t = req['type']
 
         if t == 'CREATE':
-            role = req['role']
-            store = buyers if role == 'buyer' else sellers
+            store = buyers if req['role']=='buyer' else sellers
             store[req['user']] = req['pwd']
-            return {'status': 'OK'}
+            if req['role']=='seller':
+                seller_feedback[req['user']] = [0,0]
+            return {'status':'OK'}
 
         if t == 'LOGIN':
-            store = buyers if req['role'] == 'buyer' else sellers
+            store = buyers if req['role']=='buyer' else sellers
             if store.get(req['user']) == req['pwd']:
                 sid = uuid.uuid4().hex
-                sessions[sid] = {'user': req['user'], 'role': req['role'], 'last': time.time()}
-                carts.setdefault(req['user'], {})
-                purchases.setdefault(req['user'], [])
-                return {'status': 'OK', 'sid': sid}
-            return {'status': 'ERR'}
+                sessions[sid]={'user':req['user'],'role':req['role'],'last':time.time()}
+                carts.setdefault(req['user'],{})
+                purchases.setdefault(req['user'],[])
+                return {'status':'OK','sid':sid}
+            return {'status':'ERR'}
 
         if t == 'VALIDATE':
             s = sessions.get(req['sid'])
-            if not s:
-                return {'status': 'ERR'}
-            s['last'] = time.time()
-            return {'status': 'OK', 'user': s['user']}
+            if not s: return {'status':'ERR'}
+            s['last']=time.time()
+            return {'status':'OK','user':s['user'],'role':s['role']}
 
         if t == 'ADD_CART':
-            carts[req['user']][req['item']] = carts[req['user']].get(req['item'], 0) + req['qty']
-            return {'status': 'OK'}
+            carts[req['user']][req['item']] = carts[req['user']].get(req['item'],0)+req['qty']
+            return {'status':'OK'}
+
+        if t == 'REMOVE_CART':
+            carts[req['user']][req['item']] -= req['qty']
+            return {'status':'OK'}
 
         if t == 'GET_CART':
-            return {'status': 'OK', 'cart': carts.get(req['user'], {})}
+            return {'status':'OK','cart':carts[req['user']]}
 
         if t == 'CLEAR_CART':
-            carts[req['user']] = {}
-            return {'status': 'OK'}
+            carts[req['user']]={}
+            return {'status':'OK'}
 
         if t == 'ADD_PURCHASE':
             purchases[req['user']].append(req['item'])
-            return {'status': 'OK'}
+            return {'status':'OK'}
 
         if t == 'GET_PURCHASES':
-            return {'status': 'OK', 'items': purchases.get(req['user'], [])}
+            return {'status':'OK','items':purchases[req['user']]}
 
-        return {'status': 'ERR'}
+        if t == 'SELLER_FEEDBACK':
+            fb=seller_feedback[req['seller']]
+            fb[0 if req['vote']=='up' else 1]+=1
+            return {'status':'OK'}
 
+        if t == 'GET_SELLER_RATING':
+            return {'status':'OK','rating':seller_feedback.get(req['seller'],[0,0])}
 
-threading.Thread(target=cleanup_sessions, daemon=True).start()
-s = socket.socket(); s.bind((HOST, PORT)); s.listen()
-print('Customer DB running')
+        return {'status':'ERR'}
+
+def handle(conn):
+    with conn:
+        while True:
+            req=recv(conn)
+            if not req: break
+            send(conn,process(req))
+
+threading.Thread(target=cleanup,daemon=True).start()
+s=socket.socket(); s.bind((HOST,PORT)); s.listen()
+print("Customer DB running")
 while True:
-    c,_ = s.accept()
-    threading.Thread(target=handle, args=(c,), daemon=True).start()
-
+    c,_=s.accept()
+    threading.Thread(target=handle,args=(c,),daemon=True).start()
